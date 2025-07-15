6d3d3f3bec9759c0b866585d02793d643c1ff8d6
Hars-jain <harshithjainpatwa@gmail.com>
2025-07-15 22:36:26 +0530

    Modified App.java to test CI

--- App.java
+++ App.java
@@ -3,5 +3,6 @@ package com.example;
 public class App { 
     public static void main(String[] args) { 
         System.out.println("Hello, World from Maven Project!"); 
+	System.out.println("Updated Code - CI working!");
     } 
 }
